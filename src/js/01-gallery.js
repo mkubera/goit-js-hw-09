f3b2b013d6511d0268a248c1f541d4816f1c2cf8
PawelS1988 <szatkowskipawel1988@gmail.com>
2021-10-11 12:17:19 +0200

    Code modified

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,25 @@
+import _default from '../../node_modules/simplelightbox/dist/simple-lightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+
+const createGallery = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<a class="gallery__item" href=${original}>
+      <img class="gallery__image" src=${preview} alt=${description}>
+    </a>`,
+  )
+  .join('');
+
+gallery.insertAdjacentHTML('afterbegin', createGallery);
+let gallerySL = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
