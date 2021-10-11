c13f7b33e8d4a104642a6800bf48a9d176ee2d0e
PawelS1988 <szatkowskipawel1988@gmail.com>
2021-10-11 12:03:59 +0200

    Added the code

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,41 @@
+var throttle = require('lodash.throttle');
 
+const form = document.querySelector('.feedback-form');
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+
+updateForm();
+
+form.addEventListener('input', throttle(saveEmailAndMessage, 500));
+
+function saveEmailAndMessage(evt) {
+  const data = {
+    email: form.elements.email.value,
+    message: form.elements.message.value,
+  };
+  const dataJSON = JSON.stringify(data);
+  localStorage.setItem(LOCALSTORAGE_KEY, dataJSON);
+}
+
+function updateForm() {
+  if (localStorage.getItem(LOCALSTORAGE_KEY) === null) {
+    return;
+  } else {
+    form.elements.email.value =
+      JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)).email || '';
+    form.elements.message.value =
+      JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)).message || '';
+  }
+}
+
+form.addEventListener('submit', onSubmit);
+
+function onSubmit(evt) {
+  evt.preventDefault();
+  console.log(
+    '%cParsed JSON from localStorage',
+    'color: pink; font-weight: bold',
+    JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)),
+  );
+  localStorage.removeItem(LOCALSTORAGE_KEY);
+  form.reset();
+}
